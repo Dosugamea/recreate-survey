83ab9b74fc3842619602149e32656a106c5e6ea9
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2025-12-16 01:13:56 +0900

    fix: better pc layout at admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,15 +7,13 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col md:flex-row">
-      <div className="flex items-center border-b bg-background px-4 py-3 md:hidden">
+    <div className="flex min-h-screen flex-col lg:flex-row">
+      <div className="flex items-center border-b bg-background px-4 py-3 lg:hidden">
         <AdminMobileNav />
         <span className="text-lg font-semibold">Admin</span>
       </div>
-      <div className="hidden w-64 md:block relative">
-        <Sidebar className="fixed left-0 top-0 bottom-0 w-64 border-r bg-gray-100/40" />
-      </div>
-      <div className="flex-1 p-4 md:p-8 md:ml-64 box-border min-w-0">
+      <Sidebar className="hidden lg:block fixed left-0 top-0 bottom-0 w-64 border-r bg-gray-100/40" />
+      <div className="flex-1 p-4 md:p-6 lg:p-8 lg:ml-64 box-border min-w-0">
         {children}
       </div>
     </div>
