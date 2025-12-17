bd7e9fca6aac94965f2bee8c452e04df2f96539f
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2025-12-18 00:39:42 +0900

    refactor: add eslint+prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
@@ -8,6 +10,25 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettierConfig,
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  // Storybook configuration for story files and .storybook directory
+  {
+    files: ["**/*.stories.@(js|jsx|mjs|ts|tsx)", ".storybook/**/*"],
+    plugins: {
+      storybook,
+    },
+    rules: {
+      ...storybook.configs.recommended.rules,
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
